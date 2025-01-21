968abca0dd91e48d7c391f57ef939ca2adc2923b
soosoh <hydrodddd@gmail.com>
2025-01-21 18:51:47 +0900

    serve counting

--- first.js
+++ first.js
@@ -1,33 +1,12 @@
-let win;
+let win = 11;
 
 targ = document.getElementById("target");
 start = document.querySelector("#start");
-ists = document.querySelector("#toggleSwitch");
 tsin = document.querySelector("#target");
 isduce = document.querySelector("#duceSwitch");
 
-function checkts() {
-    if (ists.checked == false) {
-        if (!tsin.classList.contains("none")) {
-            tsin.classList.add("none");
-        }
-    } else {
-        if (tsin.classList.contains("none")) {
-            tsin.classList.remove("none");
-        }
-    }
-    window.setTimeout(checkts, 100);
-}
-
 start.addEventListener("click", function () {
-    if (ists.checked == true) {
-        win = targ.value;
-    } else {
-        win = null;
-    }
     const duce = isduce.checked;
     const queryString = `win=${win}&duce=${duce}`;
     window.location.href = "scores.html?" + queryString;
 });
-
-checkts();
--- script.js
+++ script.js
@@ -13,30 +13,26 @@ function win2() {
     s2.removeEventListener("click", adds2);
 }
 
-function getQueryParam(param) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(param);
-}
+url = new URLSearchParams(window.location.search);
 
-win = parseInt(getQueryParam("win"));
-duce = getQueryParam("duce") === 'true';
+win = Number(url.get("win"));
+duce = url.get("duce") === 'true';
 
 function adds1() {
     n1++;
     s1.innerHTML = n1;
     if (n1 == win) {
-        setTimeout(win1, 1000);
+        setTimeout(win1, 500);
     } else if (n1 == win - 1 && n2 == win - 1 && duce) {
         win++;
     }
-    console.log(win);
 }
 
 function adds2() {
     n2++;
     s2.innerHTML = n2;
     if (n2 == win) {
-        setTimeout(win2, 1000);
+        setTimeout(win2, 500);
     } else if (n1 == win - 1 && n2 == win - 1 && duce) {
         win++;
     }
@@ -60,8 +56,29 @@ s1 = document.querySelector("#score1");
 s2 = document.querySelector("#score2");
 m1 = document.querySelector("#minus1");
 m2 = document.querySelector("#minus2");
+serve1 = document.querySelector("#serve1");
+serve2 = document.querySelector("#serve2");
 
 s1.addEventListener("click", adds1);
 s2.addEventListener("click", adds2);
 m1.addEventListener("click", mins1);
 m2.addEventListener("click", mins2);
+
+function servecheck(){
+    if ((n1+n2)%2==0){
+        if((n1+n2)%4==0){
+            if(serve1.classList.contains("none")){
+                serve1.classList.remove("none");
+                serve2.classList.add("none");
+            }
+        }
+        else{
+            if(serve2.classList.contains("none")){
+                serve1.classList.add("none");
+                serve2.classList.remove("none");
+            }
+        }
+    }
+}
+
+setInterval(servecheck, 100);
